92f2acab1da08d3e651f77c8441fe9db21622ebb
lazyofficial <127285528+lazyofficial@users.noreply.github.com>
2023-03-10 14:45:51 +0330

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = "6080435999:AAEANx6q5-9wr-9g2Rm2QDp6r9jdoBLqNfc"
+    TG_TOKEN = "6165223915:AAHASyQDfR4wdsNRLw_P7G5Jh2skHIlFLhM"
     
     
     
